6aac0e2881268c8fe54f7c71c914b09e761f7a53
steven wang <swang@panyi.ai>
2019-02-05 09:45:47 -0800

    add opencl support

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# assumes:
+# - you've ready run install_deps.sh
+# - you've already acitvated your python3 virtual environment
+
+# it will build coriander, tensorflow, wheel
+#
+# wont install the wheel. wont install the pre-requisites
+#
+# the wheel will be created in soft/tensorflowpkg subidrecotry
+
+set -e
+set -x
+
+. ~/conda3/bin/activate
+CLANG_HOME=$PWD/soft/llvm-4.0 util/build_coriander.sh
+#util/run_configure.sh
+util/build_tf.sh
+util/build_wheel.sh
---
+++ tensorflow/stream_executor/cl/cl_platform_id.cc
@@ -0,0 +1,28 @@
+/* Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/stream_executor/cl/cl_platform_id.h"
+
+//namespace perftools {
+//namespace gputools {
+namespace stream_executor {
+namespace cl {
+
+PLATFORM_DEFINE_ID(kClPlatformId);
+
+}  // namespace cl
+}  // namespace stream_executor
+//}  // namespace gputools
+//}  // namespace perftools
